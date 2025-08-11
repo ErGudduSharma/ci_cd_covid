cfee10f7ec4bc8a0f9700229f4fc723c3935225e
guddu <guddusharma0071@gmail.com>
2025-08-11 09:46:10 +0530

    new update

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('covid_toy.csv')
+df = pd.read_csv('insurance.csv')
+print(df.head())
 
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(strategy='mean')    
-df['fever'] = imputer.fit_transform(df[['fever']])
+print(df.info())
+print(" ")
 
+print(df.describe())
+print(" ")
+print(df.isnull().sum())
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
@@ -18,8 +21,8 @@ for col in df.columns:
 print(df.head())
 
 from sklearn.model_selection import train_test_split
-x = df.drop(columns='has_covid', axis=1)
-y = df['has_covid']
+x = df.drop(columns='charges', axis=1)
+y = df['charges']
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
@@ -34,29 +37,29 @@ x_train_new = pd.DataFrame(x_train_sc, columns = x_train.columns)
 print(np.round(x_train_new.describe(), 2))
 
 
-from sklearn.linear_model import LogisticRegression
-lr = LogisticRegression()
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
 lr.fit(x_train, y_train)
 y_pred = lr.predict(x_test)
 
-from sklearn.metrics import accuracy_score
-print('pridicted accuracy:', accuracy_score(y_test, y_pred))
+from sklearn.metrics import r2_score
+print('pridicted accuracy:', r2_score(y_test, y_pred))
 
-from sklearn.tree import DecisionTreeClassifier
-dt = DecisionTreeClassifier()
-dt.fit(x_train, y_train)
-y_pred = dt.predict(x_test)
+from sklearn.tree import DecisionTreeRegressor
+dc = DecisionTreeRegressor()
+dc.fit(x_train, y_train)
+y_pred = dc.predict(x_test)
 
-from sklearn.metrics import accuracy_score
-print('pridicted accuracy:', accuracy_score(y_test, y_pred))
+from sklearn.metrics import r2_score
+print('pridicted accuracy:', r2_score(y_test, y_pred))
 
 
-from sklearn.ensemble import RandomForestClassifier
-rf = RandomForestClassifier()
+from sklearn.ensemble import RandomForestRegressor 
+rf = RandomForestRegressor()
 rf.fit(x_train, y_train)
 y_pred = rf.predict(x_test)
 
-from sklearn.metrics import accuracy_score
-print('pridicted accuracy:', accuracy_score(y_test, y_pred))
+from sklearn.metrics import r2_score
+print('pridicted accuracy:', r2_score(y_test, y_pred))
 
 
